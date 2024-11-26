986b6b80cd15798346f0597c1e13b2a21dceaaac
Karan Dodeja <41475214+Karan-Dodeja@users.noreply.github.com>
2024-11-26 23:20:37 +0530

    Created tickets schema

--- convex/Schema.ts
+++ convex/Schema.ts
@@ -12,5 +12,18 @@ export default defineSchema({
         userId: v.string(),
         imageStorageId: v.optional(v.id("_storage")),
         is_cancelled: v.optional(v.boolean()),
-    })
+    }),
+    tickets: defineTable({
+        eventId: v.id("events"),
+        userId: v.string(),
+        purchasedAt: v.number(),
+        status: v.union(
+            v.literal("valid"),
+            v.literal("used"),
+            v.literal("refunded"),
+            v.literal("cancelled")
+        ),
+        paymentIntentId: v.optional(v.string()),
+        amount: v.optional(v.number())
+    }),
 })
