d798016838462f6da6a1d51720c1bc31e5b6ef45
Karan Dodeja <41475214+Karan-Dodeja@users.noreply.github.com>
2024-11-30 17:25:53 +0530

    Created Event List Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import EventList from "@/components/EventList";
 import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <div className="">
       <Header />
-      <h1>Welcome to event booking</h1>
+      <EventList />
     </div>
   );
 }
---
+++ components/EventList.tsx
@@ -0,0 +1,90 @@
+"use client";
+import { api } from '@/convex/_generated/api';
+import { useQuery } from 'convex/react';
+import Spinner from './Spinner';
+import { CalendarDays, Ticket } from 'lucide-react';
+
+
+function EventList() {
+    const events = useQuery(api.events.get);
+    if (!events) {
+        return (
+            <div className='min-h-[400px] flex items-center justify-center'>
+                <Spinner />
+            </div>
+        )
+    }
+
+    const upcomingEvents = events
+        .filter(
+            (e: { eventDate: number; }) => e.eventDate > Date.now()
+        )
+        .sort(
+            (a: { eventDate: number; }, b: { eventDate: number; }) => a.eventDate - b.eventDate
+        )
+
+    const pastEvents = events
+        .filter(
+            (e: { eventDate: number; }) => e.eventDate <= Date.now()
+        )
+        .sort(
+            (a: { eventDate: number; }, b: { eventDate: number; }) => b.eventDate - a.eventDate
+        )
+
+    return (
+        <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12'>
+            {/* Header */}
+            <div >
+                <h1 className='text-3xl font-bold text-gray-900'>Upcoming Events</h1>
+                <p className='mt-2 text-gray-600'>
+                    Discover & book tickets for amazing events
+                </p>
+            </div>
+            <div className='bg-white px-4  py-2 rounded-lg shadow-sm border border-gray-100'>
+                <div className='flex items-center gap-2 text-gray-600'>
+                    <CalendarDays className='w-5 h-5' />
+                    <span className='font-medium'>
+                        {upcomingEvents.length} Upcoming Events
+                    </span>
+                </div>
+            </div>
+
+            {/* Upcoming Events */}
+            {upcomingEvents.length > 0 ? (
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-12'>
+                    {
+                        upcomingEvents.map((e: { _id: any; }) => (
+                            <EventCard key={e._id} eventId={e._id} />
+                        ))
+                    }
+                </div>
+            ) : (
+                <div className='bg-gray-50 rounded-lg p-12 text-center mb-12'>
+                    <Ticket className='w-12 text-gray-400 mx-auto mb-4' />
+                    <h3 className='text-lg font-medium text-gray-900'>
+                        No upcoming Events
+                    </h3>
+                    <p className='text-gray-600 mt-1'>Check back later for new events</p>
+                </div>
+            )}
+
+            {/* Past events */}
+            {pastEvents.length > 0 && (
+                <>
+                    <h2 className='text-2xl font-bold text-gray-900 mb-6'>
+                        Past Events
+                    </h2>
+                    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+                        {
+                            pastEvents.map((e: { _id: any; }) => (
+                                <EventCard key={e._id} eventId={e._id} />
+                            ))
+                        }
+                    </div>
+                </>
+            )}
+        </div>
+    )
+}
+
+export default EventList
